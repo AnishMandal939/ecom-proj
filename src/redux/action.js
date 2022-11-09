bd0d5a0e2687e312791856e82f945770005f0143
AnishMandal939 <mandalktaanees12@gmail.com>
2022-11-09 09:12:44 +0545

    part 2 - work with actions

---
+++ src/redux/action.js
@@ -0,0 +1,15 @@
+// action is a plain simple js function
+
+// action created
+export const addToCart = (product) =>{
+    console.warn("abc action called", product)
+    // return 'abc'; - without passing type once you click to add to cart you will get error in console
+    // using type
+    return {
+        type: 'ADD_TO_CART',
+        // now directly sending data , but we'll use saga, all logic in future session
+        data: product
+    }
+}
+
+// to call action go to view files eg: App.js
